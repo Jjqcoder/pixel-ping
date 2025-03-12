23dadd1f8f2665eb0f8110b30aff1923e91c1643
江建清 <1810422805@qq.com>
2025-03-12 08:53:20 +0800

    feat:Enable cross-domain on the backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 // src/app.ts
 import express, {json, urlencoded} from "express";
 import { RegisterRoutes } from "../build/routes";
+import cors from 'cors'
 
 export const app = express();
+
+// 开启跨域
+app.use(cors())
 
 // Use body parser to read sent json payloads
 app.use(
