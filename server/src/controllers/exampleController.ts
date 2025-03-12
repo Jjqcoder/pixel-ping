b8550cece234a9103327a2e5bdca5a485d188575
江建清 <1810422805@qq.com>
2025-03-12 12:00:31 +0800

    feat:Adjust the project structure and remove unnecessary code

--- server/src/controllers/exampleController.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Route } from "tsoa";
-
-@Route("example")
-export class ExampleController extends Controller {
-  @Get()
-  public async getExample(): Promise<string> {
-    return "Hello, TSOA!";
-  }
-}
--- server/src/utils/prisma/index.ts
+++ server/src/utils/prisma/index.ts
@@ -1,3 +1,9 @@
+/**
+ * 创建时间: 2025-03-12
+ * 作者: jjq
+ * 描述: 返回PrismaClient对象工具
+ */
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
