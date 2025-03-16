e45ecb21264df3221c142cce0041e18018957727
江建清 <1810422805@qq.com>
2025-03-17 00:47:45 +0800

    chore:Set up the project to print ASCII WordArt after it starts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,12 @@ import { httpServer } from "./app";
 const port = process.env.PORT || 3000;
 
 // 注意 使用httpServer监听端口 而不是app
-httpServer.listen(port, () =>
-  console.log(`Testing at http://localhost:${port}/users`)
+httpServer.listen(port, () => {
+  console.log("        _          _             _              ");
+  console.log("  _ __ (_)_  _____| |      _ __ (_)_ __   __ _ ");
+  console.log(" | '_ \\| \\ \\/ / _ \\ |_____| '_ \\| | '_ \\ / _` |");
+  console.log(" | |_) | |>  <  __/ |_____| |_) | | | | | (_| |");
+  console.log(" | .__/|_/_/\\_\\___|_|     | .__/|_|_| |_|\\__, |");
+  console.log(" |_|                      |_|            |___/ ");
+}
 );
